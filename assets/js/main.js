9efb2730d6599a794ba4a28bceb92b1894cdbc78
Daniel <dhaavi@users.noreply.github.com>
2018-09-17 21:44:31 +0200

    Move email deobfuscation to contact page

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,26 +9,3 @@ $(document).ready(function() {
     $(".ui.sidebar").sidebar("toggle");
   })
 })
-
-// email obfuscation
-function debfus(s) {
-  var n = 0
-  var r = ""
-  for( var i = 0; i < s.length; i++)
-  {
-    n = s.charCodeAt( i )
-    if( n >= 8364 )
-    {
-      n = 128
-    }
-    r += String.fromCharCode( n - 1 )
-  }
-  return r
-}
-document.addEventListener('DOMContentLoaded', function () {
-  var elements = document.getElementsByClassName("contact-email")
-  var email = debfus('{{ site.emailobfuscated }}')
-  for (var i=0, len=elements.length; i<len; i++) {
-    elements[i].innerHTML = email
-  }
-})
