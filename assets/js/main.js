c6eebfb83bdddef7ea46098e46746bbc8ac72871
davegson <3080765+davegson@users.noreply.github.com>
2019-03-27 18:48:22 +0100

    Add functions to read get params of JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,18 @@ $(document).ready(function() {
     $(".ui.sidebar").sidebar("toggle");
   })
 })
+
+function getSearchParameters() {
+  var prmstr = window.location.search.substr(1);
+  return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}
+
+function transformToAssocArray( prmstr ) {
+  var params = {};
+  var prmarr = prmstr.split("&");
+  for ( var i = 0; i < prmarr.length; i++) {
+      var tmparr = prmarr[i].split("=");
+      params[tmparr[0]] = tmparr[1];
+  }
+  return params;
+}
