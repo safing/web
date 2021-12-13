abd2fd25e816c9fe8f5a4f20728d5633669f012d
Luke <16389426+LukeSeers@users.noreply.github.com>
2021-12-13 07:29:14 +0100

    Add safingTailwind-1.2

---
+++ vendor/safingTailwind/docs/js/internal/iframeRefresher.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+    SetUpIframeData()
+};
+
+function SetUpIframeData() {
+    $('.preview-panel-left').each(function(){
+        iframe = $(this).find( ".iframe" );
+        data = $(this).find( ".data" );
+        iframeContents = iframe.contents()
+        innerHtml = iframeContents.find('html');
+        innerBody = iframeContents.find('body');
+        innerHead = iframeContents.find('head');
+        innerHead.append('<link rel="stylesheet" href="../safingTailwind-1.2.css">');
+        innerBody.append(data)
+    });
+    setTimeout(function(){ 
+        SetUpIframeHeight()
+    }, 500);
+}
+
+function SetUpIframeHeight() {
+    $('.preview-panel-left').each(function(){
+        iframe = $(this).find( ".iframe" );
+        iframeContents = iframe.contents()
+        innerHtml = iframeContents.find('html');
+        iframe.height((innerHtml.height()) + 'px')
+    });
+}
